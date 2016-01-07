2e303943dd304de46d547ef73b4bec3083078d3c
Daniel Drotos <drani@mazsola.iit.uni-miskolc.hu>
2016-01-07 14:57:01 +0000

    implementation of input_avail for win version of fio

--- fwio.cc
+++ fwio.cc
@@ -1,6 +1,136 @@
 #include "fwiocl.h"
 
 
+enum e_handle_type
+cl_io::get_handle_type()
+{
+  DWORD file_type = GetFileType(handle);
+
+  switch (file_type)
+    {
+    case FILE_TYPE_CHAR:
+      {
+        DWORD err;
+
+        if (!ClearCommError(handle, &err, NULL))
+          {
+            switch (GetLastError())
+              {
+              case ERROR_INVALID_HANDLE:
+                return CH_CONSOLE;
+
+              case ERROR_INVALID_FUNCTION:
+                /*
+                 * In case of NUL device return type CH_FILE.
+                 * Is this the correct way to test it?
+                 */
+                return CH_FILE;
+
+              default:
+                //assert(false);
+		return CH_UNDEF;
+              }
+          }
+      }
+      return CH_SERIAL;
+
+    case FILE_TYPE_DISK:
+      return CH_FILE;
+    }
+
+  char sockbuf[256];
+  int optlen = sizeof(sockbuf);
+
+  if (SOCKET_ERROR != getsockopt((SOCKET)handle, SOL_SOCKET, SO_TYPE, sockbuf, &optlen) ||
+    WSAENOTSOCK != WSAGetLastError())
+    return CH_SOCKET;
+
+  //assert(false);
+  return CH_UNDEF;
+}
+
+int
+cl_io::input_avail(void)
+{
+  e_handle_type type= CH_UNDEF;
+  if (CH_UNDEF == type)
+    type = get_handle_type();
+
+  switch (type)
+    {
+    case CH_SOCKET:
+      {
+        struct timeval tv = {0, 0};
+	
+        //assert(INVALID_HANDLE_VALUE != handle);
+
+        fd_set s;
+        FD_ZERO(&s);
+        FD_SET((SOCKET)handle, &s);
+
+        int ret = select(0, &s, NULL, NULL, &tv);
+        if (SOCKET_ERROR == ret)
+          fprintf(stderr, "Can't select: %d\n", WSAGetLastError());
+
+        return ret != SOCKET_ERROR && ret != 0;
+      }
+
+    case CH_FILE:
+      return true;
+
+    case CH_CONSOLE:
+      {
+        PINPUT_RECORD pIRBuf;
+        DWORD NumPending;
+        DWORD NumPeeked;
+	
+        /*
+         * Peek all pending console events
+         */
+        if (INVALID_HANDLE_VALUE == handle ||
+	    !GetNumberOfConsoleInputEvents(handle, &NumPending) ||
+	    NumPending == 0 ||
+	    NULL == (pIRBuf = (PINPUT_RECORD)_alloca(NumPending * sizeof(INPUT_RECORD))))
+          return false;
+	
+        if (PeekConsoleInput(handle, pIRBuf, NumPending, &NumPeeked) &&
+	    NumPeeked != 0L &&
+	    NumPeeked <= NumPending)
+          {
+            /*
+             * Scan all of the peeked events to determine if any is a key event
+             * which should be recognized.
+             */
+            for ( ; NumPeeked > 0 ; NumPeeked--, pIRBuf++ )
+              {
+                if (KEY_EVENT == pIRBuf->EventType &&
+		    pIRBuf->Event.KeyEvent.bKeyDown &&
+		    pIRBuf->Event.KeyEvent.uChar.AsciiChar)
+                  return true;
+              }
+          }
+	
+        return false;
+      }
+      
+    case CH_SERIAL:
+      {
+        DWORD err;
+        COMSTAT comStat;
+	
+        bool res = ClearCommError(handle, &err, &comStat);
+        //assert(res);
+	
+        return res ? comStat.cbInQue > 0 : false;
+      }
+      
+    default:
+      //assert(false);
+      return false;
+    }
+}
+
+
 class cl_f *
 mk_io(chars fn, chars mode)
 {
--- fwiocl.h
+++ fwiocl.h
@@ -2,10 +2,30 @@
 #define FWIOCL_HEADER
 
 
+#include <winsock2.h>
+
+
+enum e_handle_type {
+  CH_UNDEF,
+  CH_FILE,
+  CH_SOCKET,
+  CH_CONSOLE,
+  CH_SERIAL
+};
+
 #include "fiocl.h"
 
 class cl_io: public cl_f
 {
+ private:
+  HANDLE handle;
+ public:
+ cl_io(): cl_f () {}
+ cl_io(chars fn, chars mode): cl_f(fn, mode) {}
+ protected:
+  enum e_handle_type get_handle_type();
+ public:
+  virtual int input_avail(void);
 };
 
 
