a1c6ed5519d28954166b90b530c2314c379e2aff
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2016-10-16 16:25:27 +0000

    abstract port hw

---
+++ sim.src/port_hw.cc
@@ -0,0 +1,17 @@
+/* $Id:$ */
+
+#include "port_hwcl.h"
+
+
+cl_port_hw::cl_port_hw(class cl_uc *auc, int aid, chars aid_string):
+  cl_hw(auc, HW_PORT, aid, aid_string)
+{
+}
+
+void
+cl_port_hw::proc_input(void)
+{
+}
+
+
+/* End of sim.src/port_hw.cc */
---
+++ sim.src/port_hwcl.h
@@ -0,0 +1,29 @@
+/* $Id:$ */
+
+#ifndef PORT_HW_HEADER
+#define PORT_HW_HEADER
+
+#include "newcmdposixcl.h"
+
+#include "hwcl.h"
+
+
+class cl_port_io: public cl_hw_io
+{
+  
+};
+
+class cl_port_hw: public cl_hw
+{
+ public:
+  class cl_memory_cell *cell_p, *bit_cells[32], *cell_in;
+ public:
+  cl_port_hw(class cl_uc *auc, int aid, chars aid_string);
+
+  virtual void proc_input(void);
+};
+
+
+#endif
+
+/* End of sim.src/port_hwcl.h */
