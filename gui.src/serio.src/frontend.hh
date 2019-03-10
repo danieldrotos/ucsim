2664edb4beecd7477510222a7df0d73dcf1e07bf
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2019-03-10 12:22:42 +0100

    fix serialview

--- gui.src/serio.src/frontend.hh
+++ gui.src/serio.src/frontend.hh
@@ -2,6 +2,9 @@
  * to emulate the serial input and output of an 8051 controller               *
  * frontend.hh - ncurses frontend                                             *
  ******************************************************************************/
+#ifndef FRONTEND_HEADER
+#define FRONTEND_HEADER
+
 #include <sys/types.h>
 #include <curses.h>
 /*#include <term.h>*/
@@ -9,35 +12,37 @@
 
 struct COORDS_S
 {
-	int min_x;
-	int max_x;
-	int min_y;
-	int max_y;
+  int min_x;
+  int max_x;
+  int min_y;
+  int max_y;
 };
 typedef struct COORDS_S COORDS;
 
 struct COORDINATES_S
 {
-	int x;
-	int y;
+  int x;
+  int y;
 };
 typedef struct COORDINATES_S COORDINATES;
 
 
 class Viewer
 {
-	public:
-		Viewer();
-		~Viewer();
-		void DrawBox(void);
-		void AddStrOutWin(char *string);
-		void GetStrInWin(char *string);
-		void AddChOutWin(char b);
-		char GetChInWin(void);
-
-	private:
-		WINDOW *inp, *outp;
-		COORDS win_c, inp_c, outp_c;
-		COORDINATES topleft, bottomright, current;
-		int middle_y, middle_x;
+public:
+  Viewer();
+  virtual ~Viewer();
+  virtual void DrawBox(void);
+  virtual void AddStrOutWin(char *string);
+  virtual void GetStrInWin(char *string);
+  virtual void AddChOutWin(char b);
+  virtual char GetChInWin(void);
+  
+private:
+  WINDOW *inp, *outp;
+  COORDS win_c, inp_c, outp_c;
+  COORDINATES topleft, bottomright, current;
+  int middle_y, middle_x;
 };
+
+#endif
