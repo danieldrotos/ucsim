e73bbed60227bb6a2597e15bc76497247c2b1e19
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2019-03-15 11:40:36 +0100

    fix of pdk merge

--- conf.mk
+++ conf.mk
@@ -2,16 +2,6 @@
 # Makefile targets to remake configuration
 #
 
-freshconf: Makefile
-
-Makefile: $(srcdir)/Makefile.in $(top_srcdir)/configure.ac
-	cd $(top_builddir) && $(SHELL) ./config.status
-
-# End of pdk.src/conf.mk
-#
-# Makefile targets to remake configuration
-#
-
 freshconf: main.mk ddconfig.h
 
 main.mk: $(srcdir)/main_in.mk config.status
