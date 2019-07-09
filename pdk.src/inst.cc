73443df2e154330fd42e7bce49894f938a518812
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2019-07-09 11:08:55 +0200

    apply changes of sdcc repo

--- pdk.src/inst.cc
+++ pdk.src/inst.cc
@@ -712,23 +712,12 @@ int cl_pdk::execute_pdk13(unsigned int code) {
   } else if (CODE_MASK(0x0B80, 0x3F)) {
     // ceqsn a, m
     int addr = code & 0x3F;
-    sub_to(regs.a, get_mem(addr));
-    if (regs.a == get_mem(addr))
-      ++PC;
-  } else if (CODE_MASK(0x1300, 0xFF)) {
-    // cneqsn a, k
-    sub_to(regs.a, code & 0xFF);
-    if (regs.a != (code & 0xFF))
-      ++PC;
-  } else if (CODE_MASK(0x0Bc0, 0x3F)) {
-    // cneqsn a, m
-    int addr = code & 0x3F;
-    sub_to(regs.a, get_mem(addr));
-    if (regs.a != get_mem(addr))
-      ++PC;
-  } else if (code == 0x0012) {
-    // izsn
-    regs.a = add_to(regs.a, 1);
+    sub_to(regs.a, get_mem(addr));
+    if (regs.a == get_mem(addr))
+      ++PC;
+  } else if (code == 0x0012) {
+    // izsn
+    regs.a = add_to(regs.a, 1);
     if (!regs.a)
       ++PC;
   } else if (CODE_MASK(0x0880, 0x3F)) {
