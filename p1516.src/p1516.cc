2ccb67b25777fedcd0ad68774a06a72348a4578e
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2020-06-22 11:44:21 +0200

    fix

--- p1516.src/p1516.cc
+++ p1516.src/p1516.cc
@@ -478,7 +478,7 @@ cl_p1516::exec_inst(void)
   cond= (code & 0xf0000000) >> 28;
   if ((cond&1) == 1)
     {
-      u8_t flag, fv, v;
+      u8_t flag= 0, fv, v;
       switch (cond>>2)
 	{
 	case 0: flag= F&S; break;
