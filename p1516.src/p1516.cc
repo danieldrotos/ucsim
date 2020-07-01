1b5b7f0bcbf8d2709ccb36acc19fe04f28ca517a
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2020-07-01 15:33:34 +0200

    inc/dec flag fix

--- p1516.src/p1516.cc
+++ p1516.src/p1516.cc
@@ -379,10 +379,10 @@ cl_p1516::inst_alu(t_mem code)
       break;
 
     case 4: // INC
-      RC[d]->W(RC[a]->R()+1);
+      RC[d]->W(inst_ad(RC[a]->R(), 1, 0));
       break;
     case 5: // DEC
-      RC[d]->W(RC[a]->R()-1);
+      RC[d]->W(inst_ad(RC[a]->R(), ~(1), 1));
       break;
 
     case 6: // AND
