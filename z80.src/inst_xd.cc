0081b160d26b7b63d2ae91ae4c836686c26dd599
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2020-07-29 13:18:49 +0200

    ticks in some z80 Xd

--- z80.src/inst_xd.cc
+++ z80.src/inst_xd.cc
@@ -40,157 +40,199 @@ cl_z80::inst_Xd_ld(t_mem code)
   switch (code) {
     case 0x21: // LD IX,nnnn
       regs_IX_OR_IY = fetch2();
-    return(resGO);
+      tick(13);
+      return(resGO);
     case 0x22: // LD (nnnn),IX
       tw = fetch2();
       store2(tw, regs_IX_OR_IY);
       vc.wr+= 2;
-    return(resGO);
+      tick(19);
+      return(resGO);
     case 0x26: // LD HX,nn
       regs_iX_h = fetch1();
-    return(resGO);
+      tick(10);
+      return(resGO);
     case 0x2A: // LD IX,(nnnn)
       tw = fetch2();
       regs_IX_OR_IY = get2(tw);
       vc.rd+= 2;
-    return(resGO);
+      tick(19);
+      return(resGO);
     case 0x2E: // LD LX,nn
       regs_iX_l = fetch1();
-    return(resGO);
+      tick(10);
+      return(resGO);
     case 0x36: // LD (IX+dd),nn
       tw = add_u16_disp(regs_IX_OR_IY, fetch());
       store1(tw, fetch());
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x44: // LD B,HX
       regs.bc.h = regs_iX_h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x45: // LD B,LX
       regs.bc.h = regs_iX_l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x46: // LD B,(IX+dd)
       regs.bc.h = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x4C: // LD C,HX
       regs.bc.l = regs_iX_h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x4D: // LD C,LX
       regs.bc.l = regs_iX_l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x4E: // LD C,(IX+dd)
       regs.bc.l = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x54: // LD D,HX
       regs.de.h = regs_iX_h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x55: // LD D,LX
       regs.de.h = regs_iX_l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x56: // LD D,(IX+dd)
       regs.de.h = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
-    case 0x5C: // LD E,H
-      regs.de.l = regs.hl.h;
-    return(resGO);
-    case 0x5D: // LD E,L
-      regs.de.l = regs.hl.l;
-    return(resGO);
+      tick(18);
+      return(resGO);
+    case 0x5C: // LD E,H   E,HX?
+      regs.de.l = regs_iX_h;//regs.hl.h;
+      return(resGO);
+    case 0x5D: // LD E,L   E,LX?
+      regs.de.l = regs_iX_l;//regs.hl.l;
+      return(resGO);
     case 0x5E: // LD E,(IX+dd)
       regs.de.l = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x60: // LD HX,B
       regs_iX_h = regs.bc.h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x61: // LD HX,C
       regs_iX_h = regs.bc.l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x62: // LD HX,D
       regs_iX_h = regs.de.h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x63: // LD HX,E
       regs_iX_h = regs.de.l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x64: // LD HX,HX
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x65: // LD HX,LX
       regs_iX_h = regs_iX_l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x66: // LD H,(IX+dd)
       regs.hl.h = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x67: // LD HX,A
       regs_iX_h = regs.raf.A;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x68: // LD LX,B
       regs_iX_l = regs.bc.h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x69: // LD LX,C
       regs_iX_l = regs.bc.l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x6A: // LD LX,D
       regs_iX_l = regs.de.h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x6B: // LD LX,E
       regs_iX_l = regs.de.l;
-    return(resGO);
-    case 0x6C: // LD LX,HX
-      regs_iX_l = regs.hl.h;
-    return(resGO);
+      tick(7);
+      return(resGO);
+    case 0x6C: // LD LX,HX ?
+      regs_iX_l = regs_iX_h;//regs.hl.h;
+      return(resGO);
     case 0x6D: // LD LX,LX
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x6E: // LD L,(IX+dd)
       regs.hl.l = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x6F: // LD LX,A
       regs_iX_l = regs.raf.A;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x70: // LD (IX+dd),B
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.bc.h);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x71: // LD (IX+dd),C
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.bc.l);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x72: // LD (IX+dd),D
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.de.h);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x73: // LD (IX+dd),E
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.de.l);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x74: // LD (IX+dd),H
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.hl.h);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x75: // LD (IX+dd),L
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.hl.l);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x77: // LD (IX+dd),A
       store1(add_u16_disp(regs_IX_OR_IY,fetch()), regs.raf.A);
       vc.wr++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0x7C: // LD A,HX
       regs.raf.A = regs_iX_h;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x7D: // LD A,LX
       regs.raf.A = regs_iX_l;
-    return(resGO);
+      tick(7);
+      return(resGO);
     case 0x7E: // LD A,(IX+dd)
       regs.raf.A = get1(add_u16_disp(regs_IX_OR_IY,fetch()));
       vc.rd++;
-    return(resGO);
+      tick(18);
+      return(resGO);
     case 0xF9: // LD SP,IX
       regs.SP = regs_IX_OR_IY;
-    return(resGO);
+      tick(9);
+      return(resGO);
   }
   return(resINV_INST);
 }
