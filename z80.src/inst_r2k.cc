e02e9737930b40d080299fbf0eea43cde540e123
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2020-09-16 08:44:57 +0200

    apply r11857

--- z80.src/inst_r2k.cc
+++ z80.src/inst_r2k.cc
@@ -38,22 +38,21 @@ unsigned   word_parity( u16_t  x ) {
 
 /******** rabbit 2000 memory access helper functions *****************/
 u32_t  rabbit_mmu::logical_addr_to_phys( u16_t logical_addr ) {
-  u32_t  phys_addr = logical_addr;
-  unsigned     segnib = logical_addr >> 12;
-  
-  if (segnib >= 0xE000)
-  {
-    phys_addr += ((u32_t)xpc) << 12;
-  }
-  else if (segnib >= ((segsize >> 4) & 0x0F))
-  {
-    phys_addr += ((u32_t)stackseg) << 12;    
-  }
-  else if (segnib >= (segsize & 0x0F))
-  {
-    phys_addr += ((u32_t)dataseg) << 12;    
-  }
-  
+  u32_t  phys_addr = logical_addr;
+  unsigned     segnib = logical_addr >> 12;
+  
+  if (segnib >= 0xe)
+  {
+    phys_addr += ((u32_t)xpc) << 12;
+  }
+  else if (segnib >= ((segsize >> 4) & 0xf))
+  {
+    phys_addr += ((u32_t)stackseg) << 12;    
+  }
+  else if (segnib >= (segsize & 0xf))
+  {
+    phys_addr += ((u32_t)dataseg) << 12;    
+  }
   return phys_addr;
 }
 
