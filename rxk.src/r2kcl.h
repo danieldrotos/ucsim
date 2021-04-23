5e9721a8ce33f21c6f62b03889e194c3555c4cab
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2021-04-23 11:23:33 +0200

    new RxK simulator

---
+++ rxk.src/r2kcl.h
@@ -0,0 +1,63 @@
+/*
+ * Simulator of microcontrollers (rxkcl.h)
+ *
+ * Copyright (C) 2020,20 Drotos Daniel, Talker Bt.
+ * 
+ * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
+ *
+ */
+
+/* This file is part of microcontroller simulator: ucsim.
+
+UCSIM is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+UCSIM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UCSIM; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA. */
+/*@1@*/
+
+#ifndef RXKCL_HEADER
+#define RXKCL_HEADER
+
+#include "uccl.h"
+#include "memcl.h"
+
+
+/*
+ * Base of RXK processor
+ */
+
+class cl_rxk: public cl_uc  
+{
+public:
+  u8_t A, X, Y, SP, CC;
+  class cl_memory_cell cA, cX, cY, cSP, cCC;
+public:
+  cl_rxk(class cl_sim *asim);
+  virtual int init(void);
+  virtual const char *id_string(void);
+  virtual void reset(void);
+  virtual void set_PC(t_addr addr);
+
+  virtual void mk_hw_elements(void);
+  virtual void make_cpu_hw(void);
+  virtual void make_memories(void);
+
+  virtual int clock_per_cycle(void) { return 1; }
+
+  virtual void print_regs(class cl_console_base *con);
+};
+
+
+#endif
+
+/* End of rxk.src/rxk.cc */
---
+++ rxk.src/rxk.cc
@@ -0,0 +1,121 @@
+/*
+ * Simulator of microcontrollers (rxk.cc)
+ *
+ * Copyright (C) 2020,20 Drotos Daniel, Talker Bt.
+ * 
+ * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
+ *
+ */
+
+/* This file is part of microcontroller simulator: ucsim.
+
+UCSIM is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+UCSIM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UCSIM; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA. */
+/*@1@*/
+
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "globals.h"
+#include "utils.h"
+
+#include "dregcl.h"
+
+#include "glob.h"
+
+#include "rxkcl.h"
+
+
+cl_rxk::cl_rxk(class cl_sim *asim):
+  cl_uc(asim)
+{
+}
+
+int
+cl_rxk::init(void)
+{
+  cl_uc::init();
+
+  xtal= 1000000;
+    
+  return 0;
+}
+
+
+const char *
+cl_rxk::id_string(void)
+{
+  return "RXK";
+}
+
+void
+cl_rxk::reset(void)
+{
+  cl_uc::reset();
+}
+
+  
+void
+cl_rxk::set_PC(t_addr addr)
+{
+  PC= addr;
+}
+
+void
+cl_rxk::mk_hw_elements(void)
+{
+  class cl_hw *h;
+  
+  cl_uc::mk_hw_elements();
+
+  add_hw(h= new cl_dreg(this, 0, "dreg"));
+  h->init();
+}
+
+void
+cl_rxk::make_cpu_hw(void)
+{
+}
+
+void
+cl_rxk::make_memories(void)
+{
+  class cl_address_space *as;
+  class cl_address_decoder *ad;
+  class cl_memory_chip *chip;
+  
+  rom= as= new cl_address_space("rom", 0, 0x10000, 8);
+  as->init();
+  address_spaces->add(as);
+
+  chip= new cl_memory_chip("rom_chip", 0x10000, 8);
+  chip->init();
+  memchips->add(chip);
+  ad= new cl_address_decoder(as= rom,
+			     chip, 0, 0xffff, 0);
+  ad->init();
+  as->decoders->add(ad);
+  ad->activate(0);
+}
+
+void
+cl_rxk::print_regs(class cl_console_base *con)
+{
+  con->dd_color("answer");
+
+  print_disass(PC, con);
+}
+
+/* End of rxk.src/rxk.cc */
