6a3bd8fed2a8e8c7014ddb158f7220a055fd9469
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2021-12-02 15:43:39 +0100

    start mem rm command set

--- sim.src/uc.cc
+++ sim.src/uc.cc
@@ -911,8 +911,8 @@ cl_uc::build_cmdset(class cl_cmdset *cmdset)
   }
 
   {
-    class cl_super_cmd *mem_create;
-    class cl_cmdset *mem_create_cset;
+    class cl_super_cmd *mem_create, *mem_remove;
+    class cl_cmdset *mem_create_cset, *mem_remove_cset;
     super_cmd= (class cl_super_cmd *)(cmdset->get_cmd("memory"));
     if (super_cmd)
       cset= super_cmd->get_subcommands();
@@ -984,6 +984,24 @@ cl_uc::build_cmdset(class cl_cmdset *cmdset)
       cmd->init();
       set_memory_help(cmd);
     }
+
+    mem_remove= (class cl_super_cmd *)cset->get_cmd("remove");
+    if (mem_remove)
+      mem_remove_cset= mem_remove->get_subcommands();
+    else {
+      mem_remove_cset= new cl_cmdset();
+      mem_remove_cset->init();
+    }
+    
+    mem_remove_cset->add(cmd= new cl_memory_remove_chip_cmd("chip", 0));
+    cmd->init();
+
+    if (!mem_remove)
+      cset->add(mem_remove= new cl_super_cmd("remove", 0, mem_remove_cset));
+    mem_remove->init();
+    mem_remove->add_name("del");
+    mem_remove->add_name("rm");
+    set_memory_remove_help(mem_remove);
   }
 
   super_cmd= (class cl_super_cmd *)(cmdset->get_cmd("history"));
