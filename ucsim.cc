cdcd402159ed4251124ca3d1ccf3ccb71d925f6d
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2023-02-02 16:17:37 +0100

    test mem setup speed

--- ucsim.cc
+++ ucsim.cc
@@ -35,15 +35,60 @@ class cl_general_uc: public cl_uc
 {
 public:
   cl_general_uc(cl_sim *s): cl_uc(s) {}
+  virtual void make_memories(void);
 };
+
+void
+cl_general_uc::make_memories(void)
+{
+  class cl_address_space *as;
+  class cl_memory_chip *ch;
+  class cl_address_decoder *ad;
+  double s, e;
+
+  s= dnow();
+  as= new cl_address_space("as", 0, 0x400000+128+4096, 8);
+  as->init();
+  address_spaces->add(as);
+
+  ch= new cl_chip8("ch", 0x400000+128+4096, 8, 0);
+  ch->init();
+  memchips->add(ch);
+  
+  ad= new cl_address_decoder(as, ch, 0, 0x400000+128+4096-1, 0);
+  ad->init();
+  as->decoders->add(ad);
+  ad->activate(0);
+  e= dnow();
+  e-= s;
+  printf("BIG: %f\n", e);
+
+  s= dnow();
+  as= new cl_address_space("as2", 0, 0x100000, 8);
+  as->init();
+  address_spaces->add(as);
+
+  ch= new cl_chip8("ch2", 0x100000, 8, 0);
+  ch->init();
+  memchips->add(ch);
+  
+  ad= new cl_address_decoder(as, ch, 0, 0x100000-1, 0);
+  ad->init();
+  as->decoders->add(ad);
+  ad->activate(0);
+  e= dnow();
+  e-= s;
+  printf("SMALL: %f\n", e);
+}
 
 class cl_general_sim: public cl_sim
 {
 public:
   cl_general_sim(cl_app *a): cl_sim(a) {}
-  virtual class cl_uc *mk_constroller(void)
+  virtual class cl_uc *mk_controller(void)
   {
-    return new cl_general_uc(this);
+    class cl_uc *uc= new cl_general_uc(this);
+    return uc;
   }
 };
 
