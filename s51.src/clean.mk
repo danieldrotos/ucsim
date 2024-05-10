b2e562e8cc0584ef7ad91c5f47ced996123f1f2a
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2024-05-10 10:52:31 +0200

    fix clean

--- s51.src/clean.mk
+++ s51.src/clean.mk
@@ -7,7 +7,7 @@ clean:
 	rm -f test_*.??* '(null).cdb' *.lnk *.ihx
 	rm -f .[a-z]*~
 	rm -f s51 s51.exe
-	rm -f ucsim_51 ucsim_51.exe
+	rm -f ucsim_51 ucsim_51.exe ucsim_i8051
 ifneq ($(shell test -f test/Makefile && echo ok), )
 	$(MAKE) -C test clean
 endif
