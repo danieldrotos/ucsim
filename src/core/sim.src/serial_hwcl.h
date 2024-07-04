2aa2724002ee4dd6f4fce3ca6ff11146d4919525
Daniel Drotos <drdani@mazsola.iit.uni-miskolc.hu>
2024-07-04 18:20:06 +0200

    update (c) comments

--- src/core/sim.src/serial_hwcl.h
+++ src/core/sim.src/serial_hwcl.h
@@ -1,9 +1,9 @@
 /*
  * Simulator of microcontrollers (sim.src/serial_hwcl.h)
  *
- * Copyright (C) 2016,16 Drotos Daniel, Talker Bt.
+ * Copyright (C) 2016,16 Drotos Daniel
  * 
- * To contact author send email to drdani@mazsola.iit.uni-miskolc.hu
+ * To contact author send email to dr.dkdb#gmail.com
  *
  */
 
