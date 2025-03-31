4dd1070340b78f582b109a8a60d0632a777b6a6f
Daniel Drotos <daniel.drotos@uni-miskolc.hu>
2025-03-31 21:55:07 +0200

    compile new files

--- src/sims/tlcs.src/objs.mk
+++ src/sims/tlcs.src/objs.mk
@@ -5,4 +5,4 @@ OBJECTS		= stlcs.o \
 		  simtlcs.o tlcs.o \
 		  inst_block.o inst_cpu_others.o inst_rot_sh.o \
 		  inst_jump.o inst_move.o inst_arith.o inst_bit.o \
-		  t870.o
+		  t870.o t870c.o t870c1.o
