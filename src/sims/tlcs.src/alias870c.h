7f1fe97c350d3ea978229f462eda39bbbc4c857c
drdani <drdani@gep43.iit.uni-miskolc.hu>
2025-04-09 15:07:26 +0200

    ld r,a

--- src/sims/tlcs.src/alias870c.h
+++ src/sims/tlcs.src/alias870c.h
@@ -5,19 +5,30 @@
 #define CPL_CF 			instruction_06
 
 // 0 10 - 0 1f
-#define LD_A_A                  instruction_10
-#define LD_A_W                  instruction_11
-#define LD_A_C                  instruction_12
-#define LD_A_B                  instruction_13
-#define LD_A_E                  instruction_14
-#define LD_A_D                  instruction_15
-#define LD_A_L                  instruction_16
-#define LD_A_H                  instruction_17
-#define LD_A_n                  instruction_18
-#define LD_W_n                  instruction_19
-#define LD_C_n                  instruction_1a
-#define LD_B_n                  instruction_1b
-#define LD_E_n                  instruction_1c
-#define LD_D_n                  instruction_1d
-#define LD_L_n                  instruction_1e
-#define LD_H_n                  instruction_1f
+#define LD_A_rA                 instruction_10
+#define LD_A_rW                 instruction_11
+#define LD_A_rC                 instruction_12
+#define LD_A_rB                 instruction_13
+#define LD_A_rE                 instruction_14
+#define LD_A_rD                 instruction_15
+#define LD_A_rL                 instruction_16
+#define LD_A_rH                 instruction_17
+#define LD_rA_n                 instruction_18
+#define LD_rW_n                 instruction_19
+#define LD_rC_n                 instruction_1a
+#define LD_rB_n                 instruction_1b
+#define LD_rE_n                 instruction_1c
+#define LD_rD_n                 instruction_1d
+#define LD_rL_n                 instruction_1e
+#define LD_rH_n                 instruction_1f
+
+// 0 40 - 0 4f
+#define LD_rA_A                 instruction_40
+#define LD_rW_A                 instruction_41
+#define LD_rC_A                 instruction_42
+#define LD_rB_A                 instruction_43
+#define LD_rE_A                 instruction_44
+#define LD_rD_A                 instruction_45
+#define LD_rL_A                 instruction_46
+#define LD_rH_A                 instruction_47
+
