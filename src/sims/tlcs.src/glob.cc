06ba6baeb19b4eac3b1a511b5b7bf0604d0cbed8
Daniel Drotos <dr.dkdb@gmail.com>
2025-04-15 10:43:21 +0200

    cpl g.b

--- src/sims/tlcs.src/glob.cc
+++ src/sims/tlcs.src/glob.cc
@@ -597,6 +597,7 @@ struct dis_entry disass_t870c[]=
       
     { 0x0000dee8, 0x0000fff8, ' ', 3, "LD PSW,'n_2'" },
 
+    { 0x0000e0e8, 0x0000f8f8, ' ', 2, "CPL 'r_0.0'.'b_1.0'" },
     { 0x0000e8e8, 0x0000f8f8, ' ', 2, "LD 'r_0.0'.'b_1.0',CF" },
 
     { 0x0000ffe8, 0x0000fff8, ' ', 2, "SWAP 'r_0.0'" },
