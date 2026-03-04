684411a32e7d8df6074bb8672fef5202a8f3b3e6
Daniel Drotos <daniel.drotos@uni-miskolc.hu>
2026-03-04 13:43:34 +0100

    rxk; general add8

--- src/sims/rxk.src/rxkcl.h
+++ src/sims/rxk.src/rxkcl.h
@@ -308,6 +308,7 @@ public:
   virtual int add_hl_ss(u16_t op);
   virtual int adc_hl_ss(u16_t op);
   virtual int add8(u8_t op2, bool cy);				// 0f,4t,0r,0w
+  virtual int add8(class cl_cell16 &cRes, u8_t op1, u8_t op2, bool cy);
   virtual int add16(u16_t op1, u16_t op2, class cl_cell16 &cRes, bool cy);
   virtual int add32(u32_t op1, u32_t op2, class cl_cell32 &cRes, bool cy);
   virtual int sub8(u8_t op2, bool cy);				// 0f,4t,0r,0w
